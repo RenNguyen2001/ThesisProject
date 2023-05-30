e65733e194b6f258e8b7b7502642dc58b16d8ff7
RenNguyen2001 <126844711+RenNguyen2001@users.noreply.github.com>
2023-05-30 17:26:25 +1000

    Add files via upload
    - Fixed the incorrect order of data in the serial port array. This was solved by checking for the dummy byte. If the dummy byte is found to be the correct value, the whole serial line will be stored into a byte array. 
    - Works consistently after code recompilations.

--- UnityFiles/teensySerialComs.cs
+++ UnityFiles/teensySerialComs.cs
@@ -19,7 +19,7 @@ public class teensySerialComs : MonoBehaviour
         //Console.WriteLine("hello");   Console.ReadLine();
 
         serialObj.DataBits = 8;
-        serialObj.BaudRate = 9600;
+        serialObj.BaudRate = (int)1e6;
         
         serialObj.Open();
     }
@@ -30,30 +30,37 @@ public class teensySerialComs : MonoBehaviour
         //              WITH ARDUINO
         //size of byte = 1, char = 2, int = 4 in c#
 
-        print('\n');
-        print("packet start");
+
         //need to identify the first byte
         //perhaps use the falling edge detection of a '\n' - THIS DIDN'T WORK
         //perhaps send a dummy byte - PARTIALLY WORKED
 
-        byte[] value = new byte[7];
-        serialObj.Read(value, 0, 7);
+        byte numOfValues = 5;
+        byte numOfDirectionVectors = 3; //number of direction parameters (e.g. x, y and z)
+        byte[] value = new byte[numOfValues]; //make this the same size as the amount of data being sent from the teensy/arduino
+        byte[] directionVal = new byte[numOfDirectionVectors];  //stores the directions values x, y and z in an array
+
+        if (serialObj.ReadByte() == 0)  //if dummy byte is found, in otherwords if the value valid/correct
+        {
+            print("packet start");
+            serialObj.Read(value, 0, numOfValues);    //print("valid value found");
+        }
         
+
+        int j = 0;
         for (byte i = 0; i < value.Length; i++)
         {
             switch(value[i])    //to sort through the array to find the useful values
             {
                 case 44: break;     // ','
                 case 0x0A: break;   //'\n'
-                case 0: break;      //dummy byte
-                default: print(value[i]);   break;  //valid results/ values that we want to extract
+                case 0:    break;   //dummy byte    
+                default: directionVal[j] = value[i]; print(directionVal[j]); j++;  break;  //valid results/ values that we want to extract
             }
             //print(value[i]);
         }
-        print("packet end");
-        print('\n'); 
-        //print ("h");
-        //
+
+        if (j == numOfDirectionVectors) { print("packet end"); print('\n'); }   //put outside loop so it only checks once and doesn't slow down code
     }
 
     
@@ -76,4 +83,9 @@ public class teensySerialComs : MonoBehaviour
     Used a dummy byte, this partially removed the problem. However after each successfull print, there is
     an array print of just 0s along with the dummy byte.
     For example let the dummy byte be 10, the print would be: 10, 0, 0, 0, 0, 0
+
+    30/05
+    -Added an if statement to check if the first byte is the dummy byte. Only then
+    will the whole line be read into a byte array.
+    This has solved the error on 29/05 completely
 */
